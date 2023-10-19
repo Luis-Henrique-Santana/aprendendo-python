fdeab685f6c888b429fc52e85e2265d2cf31ecb6
Luis-Henrique-Santana <lhs3000t@gmail.com>
2023-10-18 23:13:38 -0300

    Sequencia de fibonacci

--- index.py
+++ index.py
@@ -1,4 +1,13 @@
-import math
-raiz_quadrada = math.sqrt(25)
+#codigo de fib
 
-print(raiz_quadrada)
+n1 = 1
+n2 = 1
+aux = 0
+cont = 0
+
+while cont<5:
+    print(n2,' ')
+    aux = n1+n2
+    n1 = n2
+    n2 = aux
+    cont = cont+1
