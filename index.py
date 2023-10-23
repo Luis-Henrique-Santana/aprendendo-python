f529ae00ef068bd3b06f56e8bb3d59b232038adf
Luis-Henrique-Santana <lhs3000t@gmail.com>
2023-10-23 19:45:18 -0300

    Documentação + melhora de processos

--- index.py
+++ index.py
@@ -1,13 +1,16 @@
 #codigo de fib
 
-n1 = 0
-n2 = 1
-aux = 0
-cont = 0
+#n1 = 0   #cria variavel 1
+#n2 = 1 # cria variavel 2
+#aux = 0  #Cria variavel auxiliar
+#cont = 0  #Cria variavel contador
 
-while cont<50:
-    print(n2,' ')
-    aux = n1+n2
-    n1 = n2
-    n2 = aux
-    cont = cont+1
+n1, n2, aux, cont = 0, 1, 0, 0 #cria todas em sequência
+
+while cont<50:  #inicia o laço de repetição da sequencia
+    print(n2,' ')  # imprime o valor da variavel 2
+    aux = n1+n2  # soma a variavel 1 e 2 na auxiliar
+    n1 = n2    # transforma a variavel 1 na 2
+    n2 = aux  # coloca a auxiliar na variavel 2
+    cont = cont+1 # adiciona 1 no contador
+
