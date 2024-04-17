505ee787ff1e861120f569b4e84105c57b13f029
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-04-17 20:14:03 -0300

    Adição de dica de numero, se foi maior ou menor e uma melhora na visualização do codigo

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -6,19 +6,26 @@ print("**************************************")
 
 numero_secreto = random.randint(1,99)
 
-print("DICA:", numero_secreto+1)
-
 tentativa = input("Digite o numero que você acha que é o correto:")
-
 
 tentativa = int(tentativa)
 
 print("Você digitou ", tentativa)
 
-if(tentativa == numero_secreto):
+acertou = tentativa == numero_secreto
+menor = tentativa < numero_secreto
+maior = tentativa > numero_secreto
+
+if(acertou):
     print ("ACERTOU!!!!")
 
 else:
     print("Errou!!!!")
+    
+    if(maior):
+        print("Sua tentativa foi maior que o numero secreto")
+    else:
+        print("Sua tentativa foi menor que o numero secreto")    
+    
     
 print("...FIM...")
