83d906c67fe98af1e0f7963d0207a75135f9e93c
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-04-17 20:45:42 -0300

    Criação de um laço de repetição que dura enquanto tiver vidas restantes, parando e exibindo no final a quantidade de vidas restantes (em caso de acerto)

--- Jogos/adivinhacao.py
+++ Jogos/adivinhacao.py
@@ -4,28 +4,38 @@ print("**************************************")
 print("---->xX Bem vindo ao jogo de Adivinhação Xx<----")
 print("**************************************")
 
+
 numero_secreto = random.randint(1,99)
+vidas = 20
 
-tentativa = input("Digite o numero que você acha que é o correto:")
 
-tentativa = int(tentativa)
+while (vidas > 0):
+    print("Vidas restantes: {}".format(vidas))
+    tentativa = input("Digite o numero que você acha que é o correto:")
 
-print("Você digitou ", tentativa)
+    tentativa = int(tentativa)
 
-acertou = tentativa == numero_secreto
-menor = tentativa < numero_secreto
-maior = tentativa > numero_secreto
+    print("Você digitou ", tentativa)
 
-if(acertou):
-    print ("ACERTOU!!!!")
+    acertou = tentativa == numero_secreto
+    menor = tentativa < numero_secreto
+    maior = tentativa > numero_secreto
 
-else:
-    print("Errou!!!!")
-    
-    if(maior):
-        print("Sua tentativa foi maior que o numero secreto")
+    if(acertou):
+        print ("ACERTOU!!!!")
+        break; 
+
     else:
-        print("Sua tentativa foi menor que o numero secreto")    
-    
-    
+        print("Errou!!!!")
+        
+        if(maior):
+            print("Sua tentativa foi maior que o numero secreto")
+        else:
+            print("Sua tentativa foi menor que o numero secreto") 
+
+        vidas = vidas - 1  
+          
+        
+
+print("Vidas restantes: ", vidas)    
 print("...FIM...")
