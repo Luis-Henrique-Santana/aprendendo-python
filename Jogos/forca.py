a561436851f401da1d7b287aa895cfe972116405
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-04-18 22:36:40 -0300

    Tratando do formato dos caracteres da forca

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -9,8 +9,15 @@ def jogar():
    
    #enquanto não ganhou ou perdeu
     while(not enforcou and not acertou):
-        print("")
-       
+        chute = input("Desce a LETRAAAA: ")
+        chute = chute.strip()
+        
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("encontrou a letra {} na posição {}".format(letra, index))
+                index = index + 1
 
 print("...FIM...")
     
