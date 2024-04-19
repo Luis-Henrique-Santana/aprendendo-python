e9a36f77492c1ef510125e660de44932d93ffdfc
Luis-Henrique-Santana <lhs3000t@gmail.com>
2024-04-18 21:56:42 -0300

    Criando o loop de gameplay

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -3,7 +3,20 @@ def jogar():
     print("---->xX Bem vindo ao jogo de Forca Xx<----")
     print("**************************************")
 
+    palavra_secreta = "batata"
+    enforcou = False   
+    acertou = False
+   
+   #enquanto não ganhou ou perdeu
+    while(not enforcou and not acertou):
+        print("")
+       
 
-
+print("...FIM...")
     
-    print("...FIM...")
+    
+    
+    
+    
+if(__name__ == "__main__"):
+    jogar()
